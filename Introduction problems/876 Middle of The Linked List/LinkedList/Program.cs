3af78ffce6fdfe2cda086af144acb9f33a8c7b13
Owen Lindsey <Owen.lindsey98@outlook.com>
2024-09-28 18:02:06 -0700

    adding linked list problems with solutions

---
+++ Introduction problems/876 Middle of The Linked List/LinkedList/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace LinkedList
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            if (head == null)
+                throw new ArgumentException("List is empty");
+
+            int count = 0;
+            ListNode countNode = head;
+            while (countNode != null && count <= 100)
+            {
+                count++;
+                countNode = countNode.next;
+            }
+
+            if (count < 1 || count > 100)
+                throw new ArgumentException("Number of nodes must be between 1 and 100");
+
+            ListNode slow = head;
+            ListNode fast = head;
+            
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            
+            return slow;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            // Test case 1: Odd number of nodes
+            Console.WriteLine("Test case 1: Odd number of nodes");
+            ListNode head1 = CreateLinkedList(new int[] { 1, 2, 3, 4, 5 });
+            TestMiddleNode(solution, head1);
+
+            // Test case 2: Even number of nodes
+            Console.WriteLine("\nTest case 2: Even number of nodes");
+            ListNode head2 = CreateLinkedList(new int[] { 1, 2, 3, 4, 5, 6 });
+            TestMiddleNode(solution, head2);
+
+            // Test case 3: Single node
+            Console.WriteLine("\nTest case 3: Single node");
+            ListNode head3 = CreateLinkedList(new int[] { 1 });
+            TestMiddleNode(solution, head3);
+
+            // Test case 4: 100 nodes (maximum allowed)
+            Console.WriteLine("\nTest case 4: 100 nodes");
+            int[] hundredNodes = new int[100];
+            for (int i = 0; i < 100; i++) hundredNodes[i] = i + 1;
+            ListNode head4 = CreateLinkedList(hundredNodes);
+            TestMiddleNode(solution, head4);
+
+            // Uncomment to test error case:
+            // Test case 5: More than 100 nodes (should throw an exception)
+            // Console.WriteLine("\nTest case 5: More than 100 nodes (should throw an exception)");
+            // int[] tooManyNodes = new int[101];
+            // for (int i = 0; i < 101; i++) tooManyNodes[i] = i + 1;
+            // ListNode head5 = CreateLinkedList(tooManyNodes);
+            // TestMiddleNode(solution, head5);
+        }
+
+        static ListNode CreateLinkedList(int[] values)
+        {
+            if (values.Length == 0) return null;
+
+            ListNode head = new ListNode(values[0]);
+            ListNode current = head;
+            for (int i = 1; i < values.Length; i++)
+            {
+                current.next = new ListNode(values[i]);
+                current = current.next;
+            }
+            return head;
+        }
+
+        static void TestMiddleNode(Solution solution, ListNode head)
+        {
+            try
+            {
+                ListNode middle = solution.MiddleNode(head);
+                Console.WriteLine($"The middle node value is: {middle.val}");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
---
+++ Introduction problems/876 Middle of The Linked List/Readme.md
@@ -0,0 +1,23 @@
+# Middle of the Linked List 
+
+### Question:
+
+- Given the head of a singly linked list, return the middle node of the linked list. 
+
+- If there are two middle nodes, return the second middle node. 
+
+### Constraints:
+- 1 <= Node.val <= 100
+- 1 <= total number of nodes <= 100
+
+### Example 1: 
+
+- Input: [1,2,3,4,5]; 
+
+- Output: [4,5,6]; 
+
+### Example 2: 
+
+- Input: [1,2,3,4,5,6]; 
+
+- Output: [4,5,6];
